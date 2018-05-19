26a9b0954ed1d08a7c64a334138490493a7b31c4
erika-n <erika.nesse@gmail.com>
2018-05-19 08:39:59 -0400

    composition 1

--- fractal-midi.py
+++ fractal-midi.py
@@ -8,20 +8,21 @@ import numpy as np
 import sys
 
 
-tracks = 4
-midifreq = np.zeros(127)
-
-a = 440
-for x in range(127):
-   midifreq[x] = (a / 32) * (2 ** ((x - 9) / 12))
+tracks = 6
 
 
 
 def  fractal_midi(transformations,   midi, totalTime, depth, time1, time2, freq1, amp1, amp2):
     if(depth <=0):
-        track = math.floor((freq1/120)*tracks)
+        est_max = 216
+        track = math.floor((freq1/est_max)*tracks) # split midi notes across tracks
+        freq = freq1 - track*(est_max/tracks)
 
-        midi.addNote(track, 9, int(freq1), int(time1*totalTime), int((time2 - time1)*totalTime), int(amp1*100))
+
+        freq += 12 + 12*track
+        if(track == 0):
+            freq -= 12 # bass track
+        midi.addNote(track, 9, int(freq), int(time1*totalTime), int((time2 - time1)*totalTime), int(amp1*100))
 
         return
     
@@ -45,23 +46,31 @@ ticks_per_quarternote = 960
 MyMIDI = MIDIFile(tracks, removeDuplicates=True, deinterleave=False,ticks_per_quarternote=ticks_per_quarternote,eventtime_is_ticks=True) 
 MyMIDI.addTempo(track, time, tempo)
 
-totalTime = ticks_per_quarternote*4*1000
-for track in range(tracks):
-    MyMIDI.addNote(track, 0, 1, totalTime-1, 1, 1)
 
-transformations = [
-	[0, 0.5, 0.25, 0, 0, 0.75],
-	[1, 0.5, 0.75, 7, 0.5, 0],
-	[0.75, 1, 0, 12, 0, 0.75]
-]
 
 ####################### futz here ####################
-depth=8
-freq =10
+measures = 100
+
+transformations = [
+	[0,     0.5,    0.25,   12,     0,      0.75],
+	[0.5,     1,    0.75,   7,      0.75,   0],
+	[0.75,  1,      0,      24,      1,     0.75],
+    [0.25,  0,      0,      0,      0.5,    0.5]
+]
+
+depth=7
+freq =0
 t1=0
 t2=1
 amp1=0
 amp2=1
+
+totalTime = ticks_per_quarternote*4*measures
+for track in range(tracks):
+    MyMIDI.addNote(track, 0, 1, totalTime-1, 1, 1)
+
+
+
 fractal_midi(transformations,  MyMIDI, totalTime, depth, t1, t2,freq,amp1, amp2)
 
 with open("fractal-midi.mid", "wb") as output_file:
