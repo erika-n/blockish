0b9eb50b31395e175b93c6e389a91497fe1b9b62
erika-n <erika.nesse@gmail.com>
2020-09-03 09:15:10 -0400

    cleaned up commented out code

--- fractal-midi.py
+++ fractal-midi.py
@@ -19,12 +19,9 @@ def  fractal_midi(transformations,   midi, totalTime, depth, time1, time2, freq1
         track = math.floor(freq1/(100)) 
         note = freqToMid(freq1)
 
-        #print(note)
 
-        #note -= track*36
         note += 18
         
-        #print(note, track, time1, int(time1*totalTime), int(amp1*100), track)
         if(note > 10 and note < 100 and track < tracks):
            
             if(time2 -time1 > 0):
@@ -39,7 +36,7 @@ def  fractal_midi(transformations,   midi, totalTime, depth, time1, time2, freq1
     
     twidth = time2 - time1
     awidth = amp2 - amp1
-    fwidth = freq2 - freq1
+
     for t in transformations:
         r = random.randint(1, 7)
         if (r == 1):
@@ -71,18 +68,8 @@ MyMIDI.addTempo(track, time, tempo)
 ####################### futz here ####################
 
 
-# transformations = [
-# 	[0.2501,      0.01,    12,   1,     0,      0.75],
-#     [0.25,     0.501,       0,   1,      0.75,   0],
-# 	[0.75,        0.25,      7,      0.5,      1,     0.75],
-#     [0.75,         0.999,      0,      0.5,      0.5,     0.75],
-# ]
 
 transformations = [
-	# [0.25,      0,    0,   0.5,     0,      0.75],
-    # [0,     0.5,       0.5,   1,      0.75,   0],
-	# [0.75,        0.25,      0.25,      0.75,      1,     0.75],
-    # [0.5,         1,      0.75,      0.25,      0.5,     0.75],
 
 #sierpinsky triad:
 	# [1, 0.5,        2,  0.5, 0.5, 0],
@@ -90,17 +77,7 @@ transformations = [
     # [0,  0.75,   1, 0.5,0.5,     0.75],
     # [1,      0.25,       3,   -2,     0.5,      0.75],
 
-#	[0.5, 0.25,        2,  2, 0.5, 0],
-	# [0, 0.5,        2, 4, 0.25, 0.75],
-    # [0,  0.5    ,   4, 8,0.5,     1],
-    # [1, 0.5    ,   3, 8,0,     0.75],
 
-    # [1, 0.5    ,   1,8, 0.5,     0],
-	# [0.25,      0.75,       2,   -2,     0.5,      0.75],
-    # [1,         0.75,      6,   -6,      0.25,  0],
-	# [0.25,        0,      3,      -3,      0.25,     0],
-    # [0.25,         1,       3,      -3,      0.5,     0],
-    # [0,           0.5,      4,      -4,       1,     0.5],
 
 
     [1,         0,      2,   6,      1,         0.5],
